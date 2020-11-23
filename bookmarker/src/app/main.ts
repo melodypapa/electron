7dbe0f5ad24cc407f5a72eddece2e7a2c9c14b3f
Ray Zhang <ray@Rays-MacBook-Air.local>
2020-11-23 21:05:36 +0800

    add styles

--- bookmarker/src/app/main.ts
+++ bookmarker/src/app/main.ts
@@ -4,5 +4,6 @@ import * as path from "path";
 app.on('ready', () => {
     console.log('Hello from Electron');
     const mainWindow = new BrowserWindow({ webPreferences: { contextIsolation: false } });
+    //const mainWindow = new BrowserWindow();
     mainWindow.webContents.loadFile(path.join(__dirname,'../../pages/index.html'));
 });
