562e33bbab25cedc652dee6bf9edaeff1554e2bc
Ray Zhang <ray@Rays-MacBook-Air.local>
2020-11-23 21:38:07 +0800

    add the node.js integration

--- bookmarker/src/app/main.ts
+++ bookmarker/src/app/main.ts
@@ -3,7 +3,12 @@ import * as path from "path";
 
 app.on('ready', () => {
     console.log('Hello from Electron');
-    const mainWindow = new BrowserWindow({ webPreferences: { contextIsolation: false } });
+    const mainWindow = new BrowserWindow({ 
+        webPreferences: { 
+            contextIsolation: false,
+            nodeIntegration: true,
+        } 
+    });
     //const mainWindow = new BrowserWindow();
     mainWindow.webContents.loadFile(path.join(__dirname,'../../pages/index.html'));
 });
